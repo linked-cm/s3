2a8c5d8288f43270eb666a2a4c6ec745791ecb66
René Verheij <email@rene.life>
2026-04-22 22:38:47 +0100

    set up single-branch publish workflow (via 'linked setup-publish')

---
+++ .changeset/initial-release.md
@@ -0,0 +1,5 @@
+---
+'@_linked/s3': patch
+---
+
+Initial release under the new publishing setup.
--- lib/cjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-import './types.js';
-import './ontologies/s3.js';
-import './shapes/S3Bucket.js';
-import './shapes/S3FileStore.js';
-import './utils/accessUrl.js';
